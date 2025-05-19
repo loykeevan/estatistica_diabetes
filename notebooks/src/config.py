9703b8f5cac45cfdce8d198e40809f83518d1fc1
Loyke Evangelista <loyke_e@hotmail.com>
2025-05-19 14:59:55 -0300

    Tratamento completo da base e transformação em um arquivo Parquet

--- notebooks/src/config.py
+++ notebooks/src/config.py
@@ -5,12 +5,15 @@ PASTA_PROJETO = Path(__file__).resolve().parents[2]
 
 PASTA_DADOS = PASTA_PROJETO / "dados"
 
-# coloque abaixo o caminho para os arquivos de dados de seu projeto
+# Caminho para os arquivos de dados de seu projeto
 DADOS_ORIGINAIS = PASTA_DADOS / "diabetes_binary_5050split_health_indicators_BRFSS2015.csv"
 
-# coloque abaixo o caminho para os arquivos de modelos de seu projeto
+# Caminho para os arquivos de dados de seu projeto
+DADOS_ORIGINAIS = PASTA_DADOS / "diabetes_tratado.parquet"
+
+# Caminho para os arquivos de modelos de seu projeto
 PASTA_MODELOS = PASTA_PROJETO / "modelos"
 
-# coloque abaixo outros caminhos que você julgar necessário
+# Caminhos que você julgar necessário
 PASTA_RELATORIOS = PASTA_PROJETO / "relatorios"
 PASTA_IMAGENS = PASTA_RELATORIOS / "imagens"
